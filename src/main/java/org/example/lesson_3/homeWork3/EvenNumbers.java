6e46520ffd98666b227fb10cd6345d40cc0c8e52
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-17 22:12:24 +0300

    Update EvenNumbers.java

--- src/main/java/org/example/lesson_3/homeWork3/EvenNumbers.java
+++ src/main/java/org/example/lesson_3/homeWork3/EvenNumbers.java
@@ -6,7 +6,7 @@ public class EvenNumbers {
     public static void main(String[] args) {
 
         for (int i = 2; i <= 100; i++){
-
+//если сделать i+=2, то и проверка не нужна, но и так ок
             if (i % 2 == 0){
                 System.out.println(i);
             }
