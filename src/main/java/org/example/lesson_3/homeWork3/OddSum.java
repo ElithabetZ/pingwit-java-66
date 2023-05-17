ff239fe8dfca41e77ee0a8fc2587a8d802f397fd
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-17 22:11:00 +0300

    Update OddSum.java

--- src/main/java/org/example/lesson_3/homeWork3/OddSum.java
+++ src/main/java/org/example/lesson_3/homeWork3/OddSum.java
@@ -6,7 +6,7 @@ public class OddSum {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 1; i <= 99; i++){
-
+//если сделать i+=2, то и проверка не нужна
             if(i % 2 != 0) {
                 sum += i;
             }
