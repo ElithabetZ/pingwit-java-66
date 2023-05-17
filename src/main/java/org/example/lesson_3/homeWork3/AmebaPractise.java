da5f77e4d841b5f05b8407900d85f897e75e7e31
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-17 22:09:30 +0300

    Update AmebaPractise.java

--- src/main/java/org/example/lesson_3/homeWork3/AmebaPractise.java
+++ src/main/java/org/example/lesson_3/homeWork3/AmebaPractise.java
@@ -3,6 +3,7 @@ package org.example.lesson_3.homeWork3;
 Одноклеточная амеба каждые 3 часа делится на 2 клетки. Определить,
 сколько амеб будет через 3, 6, 9, 12,..., 24 часа
  */
+//+
 public class AmebaPractise {
     public static void main(String[] args) {
 
