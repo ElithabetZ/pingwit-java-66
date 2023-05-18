7dd58403da30e23c9a61a93c4b1a7c62d46d4ceb
ElithabetZ <elizyshchuk@gmail.com>
2023-05-18 20:09:25 +0200

    Lesson 4

---
+++ src/main/java/org/example/lesson_4/CustomMethod.java
@@ -0,0 +1,27 @@
+package org.example.lesson_4;
+
+import java.util.Scanner;
+
+public class CustomMethod {
+    public static void main(String[] args) {
+        int max = getMaxNumber();
+        findSum(max);
+    }
+
+    public static void findSum(int maxNumber) {
+        int i = 0;
+        int sum = 0;
+        while (i <= maxNumber){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+
+    static int getMaxNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter max number: ");
+        int max = scanner.nextInt();
+        return max;
+    }
+}
---
+++ src/main/java/org/example/lesson_4/ScannerExample.java
@@ -0,0 +1,18 @@
+package org.example.lesson_4;
+
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter your name...");
+        String name = scan.next();
+
+        System.out.println("How old are you?");
+        byte age = scan.nextByte();
+
+        System.out.println("Your name is " + name);
+        System.out.println("Your age is " + age);
+    }
+}
