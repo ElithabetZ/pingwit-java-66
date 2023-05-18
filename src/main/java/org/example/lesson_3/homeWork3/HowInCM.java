ba374a5065c20aa0282750f4d45d23f5fba6bcfc
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-18 19:19:15 +0300

    Update HowInCM.java

--- src/main/java/org/example/lesson_3/homeWork3/HowInCM.java
+++ src/main/java/org/example/lesson_3/homeWork3/HowInCM.java
@@ -6,7 +6,8 @@ package org.example.lesson_3.homeWork3;
 public class HowInCM {
     public static void main(String[] args) {
 //можно удалить эту пустую строку
-        for (int i = 1; i <= 20; i++){
+        int maxIterationNum = 20;
+        for (int i = 1; i <= maxIterationNum; i++){
             double inch = 2.54;//каждый раз эту переменную создавать не надо. Она должна быть перед циклом
             double sum = i * inch;
             System.out.println(i + " inch in CM is " + sum);
