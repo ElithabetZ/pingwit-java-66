d5e4653d423fbc995cd937f9ccd60eeb53ef023f
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-25 09:45:40 +0300

    Update InvertedOrder.java

--- src/main/java/org/example/lesson_5/homeWork5/InvertedOrder.java
+++ src/main/java/org/example/lesson_5/homeWork5/InvertedOrder.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 /*
 Есть массив чисел, нужно инвертировать его порядок, т.е. массив [0,1,2,3,4] -> [4,3,2,1,0]
  */
+//+
 public class InvertedOrder {
     public static void main(String[] args) {
         int[] array = {0, 1, 2, 3, 4};
