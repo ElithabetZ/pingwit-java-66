470039fde04c7f273da83d84ca7f46e9c0c07053
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-05-25 09:41:41 +0300

    Update AverageOfNumbers.java

--- src/main/java/org/example/lesson_5/homeWork5/AverageOfNumbers.java
+++ src/main/java/org/example/lesson_5/homeWork5/AverageOfNumbers.java
@@ -3,6 +3,7 @@ package org.example.lesson_5.homeWork5;
 /*
 Есть массив чисел. Найти среднее арифметическое число элементов массива
  */
+//лишние строки...форматирование
 public class AverageOfNumbers {
     public static void main(String[] args) {
         int[] array = {2, 12, 32, 52, 62, 45};
@@ -11,6 +12,7 @@ public class AverageOfNumbers {
         System.out.println("Average of the array is " + averageArray);
     }
 
+    //лучше не get, а count
     public static int getSumOfArray(int[] array) {
         int sum = 0;
         for (int index : array) {
