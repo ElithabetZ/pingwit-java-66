5efee4936e0e3ff9bf1e92dc0fa1868dbf0bbe46
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-11 19:49:20 +0300

    Update Warehouse.java

--- src/main/java/org/example/lesson_8/home_work_8/Warehouse.java
+++ src/main/java/org/example/lesson_8/home_work_8/Warehouse.java
@@ -16,6 +16,7 @@ public class Warehouse {
 
     public Box next(){
         if(index == (boxes.length-1)){
+            //тогда индекс должен снова стартануть с 0. index = 0;
             return boxes[0];
         } else {
         Box example = boxes[++index];
