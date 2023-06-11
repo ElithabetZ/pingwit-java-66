d69c15f83fada73ea675a41888f61f633e8f6e31
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-11 19:48:32 +0300

    Update Box.java

--- src/main/java/org/example/lesson_8/home_work_8/Box.java
+++ src/main/java/org/example/lesson_8/home_work_8/Box.java
@@ -2,6 +2,7 @@ package org.example.lesson_8.home_work_8;
 
 public record Box(String name, double weight) {
 
+    //а разве этот метод в record под капотом уже не переопределен?
     @Override
     public String toString() {
         return "{name='" + name + '\'' +
