617373cf4efdbbe4430ebf44d78f8b8f70d0b6af
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-15 09:29:37 +0300

    Update Tree.java

--- src/main/java/org/example/lesson_10/home_work_10/task_2/Tree.java
+++ src/main/java/org/example/lesson_10/home_work_10/task_2/Tree.java
@@ -3,8 +3,11 @@ package org.example.lesson_10.home_work_10.task_2;
 import java.util.Arrays;
 
 public class Tree {
+    //у тебя в ветках уже фрукты, поэтому этого поля здесь не должно быть
     private Fruit name;
+    //подразумевалось, что дерево - одна ветка, ибо пользователь может создать на одном дереве ветки с разными фруктами:)
     private Branch[] branches;
+    //этого поля здесь не должно быть
     private int sumOfFruits;
 
     public Tree(Fruit name, Branch[] branches)
