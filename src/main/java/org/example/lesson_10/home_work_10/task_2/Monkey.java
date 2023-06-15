5878118762fee8b637a44e453113cb69e98056d4
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-15 09:26:29 +0300

    Update Monkey.java

--- src/main/java/org/example/lesson_10/home_work_10/task_2/Monkey.java
+++ src/main/java/org/example/lesson_10/home_work_10/task_2/Monkey.java
@@ -1,7 +1,7 @@
 package org.example.lesson_10.home_work_10.task_2;
 
 public class Monkey {
-    private Fruit typeOfMonkey;
+    private Fruit typeOfMonkey;//может фрукт? Обезьяна может уметь считать не только 1 фрукт, а несколько, а значит это не просто тип Fruit
 
     public Monkey(Fruit typeOfMonkey) {
         this.typeOfMonkey = typeOfMonkey;
