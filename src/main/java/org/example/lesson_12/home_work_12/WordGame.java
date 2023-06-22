9f3180f1c185b8a25586603f85bd62f886c6ed61
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-22 08:49:05 +0300

    Update WordGame.java

--- src/main/java/org/example/lesson_12/home_work_12/WordGame.java
+++ src/main/java/org/example/lesson_12/home_work_12/WordGame.java
@@ -3,6 +3,7 @@ package org.example.lesson_12.home_work_12;
 /*
 Игра в слова. Есть строка “engineering”. Нужно выделить подстроку так, чтобы получилось два слова “engine” и “ring”.
  */
+//+
 public class WordGame {
     public static void main(String[] args) {
         String str = "engineering";
