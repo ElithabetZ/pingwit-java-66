3035bbc678b74d9181264f24247bc9f9ca1b3e42
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-22 08:50:33 +0300

    Update MoreThanFive.java

--- src/main/java/org/example/lesson_12/home_work_12/MoreThanFive.java
+++ src/main/java/org/example/lesson_12/home_work_12/MoreThanFive.java
@@ -5,10 +5,10 @@ package org.example.lesson_12.home_work_12;
 public class MoreThanFive {
     public static void main(String[] args) {
         String[] text = {"fmeofkmoer", "fmeronfoer", "cneornvoerivore", "mferkomrel", "mfeo", "mfrk", "mkr"};
-        int moreThanNumber = 5;
-        for(int i = 0; i < text.length; i++)
+        int moreThanNumber = 5;//нейминг не очень. Лучше lengthLimit
+        for(int i = 0; i < text.length; i++)//ctrl+alt+l. 10 строка должна быть в 9
         {
-            if(text[i].length() > moreThanNumber)
+            if(text[i].length() > moreThanNumber)//форматирование
             {
                 System.out.println(text[i]);
             }
