1be94deaf1597006538d363fdda3b0f286a2d744
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-26 11:05:31 +0300

    Update Coffee.java

--- src/main/java/org/example/lesson_13/home_work_13/task_2/Coffee.java
+++ src/main/java/org/example/lesson_13/home_work_13/task_2/Coffee.java
@@ -3,6 +3,7 @@ package org.example.lesson_13.home_work_13.task_2;
 import java.util.Objects;
 
 public class Coffee extends Product{
+    //можно было тоже как enum
     private String type;
     public Coffee(String name, double price, int quantity, String type) {
         super(name, price, quantity);
