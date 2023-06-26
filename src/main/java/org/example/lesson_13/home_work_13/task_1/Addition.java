a231b7b27b88862f8682a33512856ca3d63a8d9e
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-06-26 11:03:40 +0300

    Update Addition.java

--- src/main/java/org/example/lesson_13/home_work_13/task_1/Addition.java
+++ src/main/java/org/example/lesson_13/home_work_13/task_1/Addition.java
@@ -3,6 +3,7 @@ package org.example.lesson_13.home_work_13.task_1;
 public class Addition implements Operation {
     @Override
     public double operate(double num1, double num2) {
+        //скобки необязательно
         return (num1 + num2);
     }
 }
