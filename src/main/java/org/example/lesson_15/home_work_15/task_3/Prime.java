fd836e54d24eca5c9f21077a4ef7b16f63ab81ed
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-03 10:21:45 +0300

    Update Prime.java

--- src/main/java/org/example/lesson_15/home_work_15/task_3/Prime.java
+++ src/main/java/org/example/lesson_15/home_work_15/task_3/Prime.java
@@ -1,6 +1,8 @@
 package org.example.lesson_15.home_work_15.task_3;
 
 public class Prime {
+    //find - искать. значит метод должен не boolean возвращать
+    //в таоем случае - isPrime
     public static boolean findPrime(int number) throws ExeptionPrime{
         if (number <= 1) {
             throw new ExeptionPrime("The number is less than 1", number);
