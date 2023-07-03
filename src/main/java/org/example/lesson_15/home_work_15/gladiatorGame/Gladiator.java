2c825aaeabc5c50a236884da1a6ea62b91b17437
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-03 10:46:15 +0300

    Update Gladiator.java

--- src/main/java/org/example/lesson_15/home_work_15/gladiatorGame/Gladiator.java
+++ src/main/java/org/example/lesson_15/home_work_15/gladiatorGame/Gladiator.java
@@ -2,12 +2,12 @@ package org.example.lesson_15.home_work_15.gladiatorGame;
 
 import java.util.Arrays;
 import java.util.Random;
-
+//+
 public class Gladiator {
 
     private String name;
     private String[] opponents = {"Rick", "Snake", "Death"};
-    private int opponentIndex = new Random().nextInt(opponents.length);
+    private int opponentIndex = new Random().nextInt(opponents.length);//это можно в конструктор перенести
 
     public Gladiator(String name) {
         this.name = name;
