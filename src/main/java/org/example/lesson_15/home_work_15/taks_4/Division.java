9b330ed856c7400c8aac8564a22bf60b49c1ea78
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-03 10:23:39 +0300

    Update Division.java

--- src/main/java/org/example/lesson_15/home_work_15/taks_4/Division.java
+++ src/main/java/org/example/lesson_15/home_work_15/taks_4/Division.java
@@ -4,6 +4,7 @@ import org.example.lesson_15.home_work_15.task_3.ExeptionPrime;
 
 public class Division {
 
+    //метод - действие, глагол. то есть divide
     public static void division(int numerator, int denominator) throws ExeptionDivision{
         if(denominator==0){
             throw new ExeptionDivision("The denominator can't be 0");
