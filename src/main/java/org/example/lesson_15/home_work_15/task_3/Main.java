e5ef1ddcb5ca5096a3a0f633559188dfe1164a6f
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-03 10:20:55 +0300

    Update Main.java

--- src/main/java/org/example/lesson_15/home_work_15/task_3/Main.java
+++ src/main/java/org/example/lesson_15/home_work_15/task_3/Main.java
@@ -13,7 +13,7 @@ public class Main {
             System.out.println(result);
         }
         catch(ExeptionPrime ex){
-
+//пустые строки!
             System.out.println(ex.getMessage());
             System.out.println(ex.getNumber());
         }
