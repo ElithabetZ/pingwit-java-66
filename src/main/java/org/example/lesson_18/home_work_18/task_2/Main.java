e843ba6b585c35b961375ec7fb363fb6effff492
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-13 15:34:43 +0000

    Update Main.java

--- src/main/java/org/example/lesson_18/home_work_18/task_2/Main.java
+++ src/main/java/org/example/lesson_18/home_work_18/task_2/Main.java
@@ -3,6 +3,7 @@ package org.example.lesson_18.home_work_18.task_2;
 public class Main {
 
     public static void main(String[] args) {
+        //Garage<BMW> garageBMW = new Garage<>(...)
         Garage garageBMW = new Garage<BMW>(new BMW[]{new BMW(1987), new BMW(2003)}, "Mike");
         System.out.println("BMW Garage " + garageBMW);
         Garage garageLada = new Garage<Lada>(new Lada[]{new Lada(2005), new Lada(1890)}, "Luiza");
