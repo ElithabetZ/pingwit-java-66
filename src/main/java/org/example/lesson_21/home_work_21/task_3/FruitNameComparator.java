6bd7abd6d5351fcbd43f922786154c32305c6c15
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-27 10:03:21 +0300

    Update FruitNameComparator.java

--- src/main/java/org/example/lesson_21/home_work_21/task_3/FruitNameComparator.java
+++ src/main/java/org/example/lesson_21/home_work_21/task_3/FruitNameComparator.java
@@ -1,7 +1,7 @@
 package org.example.lesson_21.home_work_21.task_3;
 
 import java.util.Comparator;
-
+//+
 public class FruitNameComparator implements Comparator<Fruit> {
     public int compare(Fruit f1, Fruit f2) {
         return f1.getName().compareTo(f2.getName());
