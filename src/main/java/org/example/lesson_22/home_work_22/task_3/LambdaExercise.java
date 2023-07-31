b683e5c8f60639c3678c43aa24561a57391cff19
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-31 18:23:40 +0300

    Update LambdaExercise.java

--- src/main/java/org/example/lesson_22/home_work_22/task_3/LambdaExercise.java
+++ src/main/java/org/example/lesson_22/home_work_22/task_3/LambdaExercise.java
@@ -3,6 +3,7 @@ package org.example.lesson_22.home_work_22.task_3;
 Создать класс User, сделать конструктор с 1 параметром, например, имя.
 Создать функциональный интерфейс, с методом, который бы создавал юзера. Написать лямбда-выражение и создать юзера
  */
+//+
 public class LambdaExercise {
     public static void main(String[] args) {
 
