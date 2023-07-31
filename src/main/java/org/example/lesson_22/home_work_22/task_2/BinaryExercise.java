52db97695bac0de4b426634dfe5d157d7184b8eb
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-07-31 18:21:25 +0300

    Update BinaryExercise.java

--- src/main/java/org/example/lesson_22/home_work_22/task_2/BinaryExercise.java
+++ src/main/java/org/example/lesson_22/home_work_22/task_2/BinaryExercise.java
@@ -6,6 +6,7 @@ import java.util.function.BinaryOperator;
 /*
 Используя интерфейс BinaryOperator написать лямбда-выражение, которое бы умножало одно целое число на другое
  */
+//+
 public class BinaryExercise {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
