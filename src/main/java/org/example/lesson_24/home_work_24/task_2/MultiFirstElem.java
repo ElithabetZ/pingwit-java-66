edaead9700aa4ffba2ce9e7158a60ce0f87c41ad
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-08-14 12:15:40 +0300

    Update MultiFirstElem.java

--- src/main/java/org/example/lesson_24/home_work_24/task_2/MultiFirstElem.java
+++ src/main/java/org/example/lesson_24/home_work_24/task_2/MultiFirstElem.java
@@ -7,12 +7,12 @@ import java.util.List;
 Есть коллекция чисел, найти произведение первых 5ти элементов.
  */
 public class MultiFirstElem {
-
+    //+
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(5, 10, 1, 2, 1, 6, 3, 100, 189);
         Integer result = numbers.stream()
                 .limit(5)
-                .reduce(1, (multiply, element) -> multiply * element);
+                .reduce(1, (multiply, element) -> multiply * element);//нейминг не очень. лучше (partial, elem). Multiply - глагол
         System.out.println(result);
     }
 }
