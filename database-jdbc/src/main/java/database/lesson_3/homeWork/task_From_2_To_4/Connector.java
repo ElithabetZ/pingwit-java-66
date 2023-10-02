d5ccf5a926e604ed99df4a75cd5a333b1dcd2b38
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-10-02 10:02:33 +0300

    Update Connector.java

--- database-jdbc/src/main/java/database/lesson_3/homeWork/task_From_2_To_4/Connector.java
+++ database-jdbc/src/main/java/database/lesson_3/homeWork/task_From_2_To_4/Connector.java
@@ -2,7 +2,29 @@ package database.lesson_3.homeWork.task_From_2_To_4;
 
 import java.sql.*;
 import java.util.List;
+/*
+    ты молодчинка, что разделила querymanager и connector.
+    Но теперь смотри: если у тебя появится новая сущность, то это все будет в коннекторе - например, findEmployees и т.д и т.п. А таких сущностей
+    может быть триллион, образно)) Представляешь, что будет с этим классом?...
+    Поэтому давай пременим все знания полиморфизма. Можно под каждую сущность создать свой интерфейс.
+    interface Repository<T> {
+        List<T> find(String query);
+    }
 
+    interface StudentRepository<Student> extends Repository {
+        ...
+    }
+
+    class StudentRepositoryImpl implements StudentRepository {
+        @Override
+        ...
+    }
+
+    И затем можно в коннекторе добавить Repository. Аналогично и с QueryManager :)
+
+    Именно такое разделение сейчас позволит тебе с легкостью расширять свое приложение и писать крутой код в будущем 😎
+*/
+    
 public class Connector {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/students";
@@ -35,6 +57,7 @@ public class Connector {
         }
     }
 
+    //ты осуществляешь поиск и возвращаешь их. Зачем в параметрах List? Создавай в методе новый
     public List<Student> findStudents(String query, List<Student> students){
         try {
             Class.forName("org.postgresql.Driver");
@@ -54,7 +77,8 @@ public class Connector {
 
             return students;
         } catch (ClassNotFoundException | SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e);//а давай так не будем делать? Я на занятии так пишу для быстроты, но и говорю, что так делать очень плохо
+            //какие есть варианты? 
         }
     }
 }
