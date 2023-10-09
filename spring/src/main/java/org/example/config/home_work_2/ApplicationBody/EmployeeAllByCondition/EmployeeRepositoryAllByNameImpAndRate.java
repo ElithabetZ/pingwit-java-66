8dbff1d5b7f6c7dd5a493075215d939779c7cfcc
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-10-09 10:40:27 +0300

    Update EmployeeRepositoryAllByNameImpAndRate.java

--- spring/src/main/java/org/example/config/home_work_2/ApplicationBody/EmployeeAllByCondition/EmployeeRepositoryAllByNameImpAndRate.java
+++ spring/src/main/java/org/example/config/home_work_2/ApplicationBody/EmployeeAllByCondition/EmployeeRepositoryAllByNameImpAndRate.java
@@ -5,10 +5,11 @@ import org.example.config.home_work_2.ApplicationBody.JDBCConnector.JDBCConnecto
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+//а потом надо будет не только по name, rate, а еще по многим полям со всевозможными комбинациями? Сколько же это классов будет??
 @Repository("getAllByNameAndRate")
 public class EmployeeRepositoryAllByNameImpAndRate implements EmployeeRepositoryCondition{
     @Override
-    public List<Employee> getAllByCondition(Employee employee) {
+    public List<Employee> getAllByCondition(Employee employee) {//Condition, Criteria
         List<Employee> employees = new JDBCConnectorEmployeeService().getAllByNameAndRate(employee.getName(), employee.getRatePerHour());
         return employees;
     }
