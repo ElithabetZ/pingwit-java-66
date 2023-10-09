5234a67d8830ec319cf902c4d751e9bac87f86de
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-10-09 10:39:27 +0300

    Update EmployeeRepositoryCondition.java

--- spring/src/main/java/org/example/config/home_work_2/ApplicationBody/EmployeeAllByCondition/EmployeeRepositoryCondition.java
+++ spring/src/main/java/org/example/config/home_work_2/ApplicationBody/EmployeeAllByCondition/EmployeeRepositoryCondition.java
@@ -3,7 +3,8 @@ package org.example.config.home_work_2.ApplicationBody.EmployeeAllByCondition;
 import org.example.config.home_work_2.ApplicationBody.Employee;
 
 import java.util.List;
-
+//нет, это один интерфейс EmployeeRepository
 public interface EmployeeRepositoryCondition {
+    //Employee - не condition. Создай для этого отдельный класс - Condition/Criteria
     List<Employee> getAllByCondition(Employee employee);
 }
