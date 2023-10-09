562ef584e145174b02acca71898e58c647275b20
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-10-09 10:38:31 +0300

    Update EmployeeRepository.java

--- spring/src/main/java/org/example/config/home_work_2/ApplicationBody/EmployeeAllRepository/EmployeeRepository.java
+++ spring/src/main/java/org/example/config/home_work_2/ApplicationBody/EmployeeAllRepository/EmployeeRepository.java
@@ -3,7 +3,7 @@ package org.example.config.home_work_2.ApplicationBody.EmployeeAllRepository;
 import org.example.config.home_work_2.ApplicationBody.Employee;
 
 import java.util.List;
-
+//в папку repository
 public interface EmployeeRepository {
     List<Employee> getAll();
 }
