da286a7ac9948d4735b2522c3a6408401f05dbc5
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-10-09 10:38:07 +0300

    Update EmployeeRepositoryAllImp.java

--- spring/src/main/java/org/example/config/home_work_2/ApplicationBody/EmployeeAllRepository/EmployeeRepositoryAllImp.java
+++ spring/src/main/java/org/example/config/home_work_2/ApplicationBody/EmployeeAllRepository/EmployeeRepositoryAllImp.java
@@ -5,12 +5,13 @@ import org.example.config.home_work_2.ApplicationBody.JDBCConnector.JDBCConnecto
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+//Репозиторий ассоциирован с методом... А если их будет 20?? 20 репозиториев? И как их менеджить...
 @Repository("getAll")
 public class EmployeeRepositoryAllImp implements EmployeeRepository{
 
     @Override
     public List<Employee> getAll() {
+        //new JDBCConnectorEmployeeService(). А бин создать? Передать его тут в конструкторе/@Autowired
         List<Employee> employees = new JDBCConnectorEmployeeService().getAll();
         return employees;
     }
