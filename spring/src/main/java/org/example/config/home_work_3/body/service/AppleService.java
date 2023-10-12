b2386c03fa67446b659c6fe4e1836817abf0d6e8
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-10-12 10:18:18 +0300

    Update AppleService.java

--- spring/src/main/java/org/example/config/home_work_3/body/service/AppleService.java
+++ spring/src/main/java/org/example/config/home_work_3/body/service/AppleService.java
@@ -9,5 +9,12 @@ public interface AppleService {
 
     List<Apple> getAll();
 
+    /*
+        List<Apple> getByCriteria(Criteria criteria);
+        Apple get(Long (или Integer - что там у тебя) id);
+    */
+    //Возвращаемый тип абсолютно ни о чем не говорит. По названию метода я предполагаю возвращение яблок.  
+    //Если ты хочешь калькулировать еще что-то, то создай для этого отдельный метод - calculateStatistics(List<Apple> apples);
+    //Не смешивай логику
     Map<String, Integer> getByName(String name);
 }
