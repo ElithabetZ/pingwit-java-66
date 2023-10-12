418cb98fea68d44e2346e1839c0ccb656eb86763
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-10-12 10:14:10 +0300

    Update AppleRepositoryImp.java

--- spring/src/main/java/org/example/config/home_work_3/body/repository/AppleRepositoryImp.java
+++ spring/src/main/java/org/example/config/home_work_3/body/repository/AppleRepositoryImp.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Repository
 public class AppleRepositoryImp implements AppleRepository{
 
-    @Qualifier("JdbcAppleConnector")
+    @Qualifier("JdbcAppleConnector")// @Qualifier("jdbcAppleConnector")
     private final JdbcConnector<Apple> jdbcConnector;
 
     public AppleRepositoryImp(JdbcAppleConnector jdbcConnector){
