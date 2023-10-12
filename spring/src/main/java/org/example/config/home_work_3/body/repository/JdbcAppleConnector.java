4ed48b26f5c9fbac043029c7004babcf5aacc4a3
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-10-12 10:13:34 +0300

    Update JdbcAppleConnector.java

--- spring/src/main/java/org/example/config/home_work_3/body/repository/JdbcAppleConnector.java
+++ spring/src/main/java/org/example/config/home_work_3/body/repository/JdbcAppleConnector.java
@@ -13,17 +13,19 @@ public class JdbcAppleConnector implements JdbcConnector<Apple>{
     private static final String URL = "jdbc:postgresql://localhost:5432/apples";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "root";
-    private static final String QUERY = "select * from apple_warehouse";
+    private static final String QUERY = "select * from apple_warehouse";//лучше без звездочки, а прописать поля
 
     //логика с коннекшеном везде будет дублироваться, поэтому ее лучше вынести в JdbcConnetror, а здесь просто переиспользовать (default метод, например)
     @Override
     public List<Apple> getAll() {
         try {
+            //в отдельный метод: Connection prepareConnection() in JdbcConnector
             Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             Statement find = connection.createStatement();
             ResultSet result = find.executeQuery(QUERY);
             List<Apple> apples = new ArrayList<>();
+            //вынеси в отдельный метод - Apple mapApple(ResultSet rawData);
             while(result.next()){
                 Long id = result.getLong(1);
                 String name = result.getString(2);
