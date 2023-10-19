38ea17c818654a9a03970330cb2407ebc4190ccc
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-10-19 09:31:34 +0300

    Update Application.java

--- spring-3-task/src/main/java/comPlayCar/Application.java
+++ spring-3-task/src/main/java/comPlayCar/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@ConfigurationPropertiesScan
+@ConfigurationPropertiesScan//я не вижу у тебя никаких @ConfigurationProperties, зачем аннотация?
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
