a8909a77741570f406ea3bb80f6f61f16d945104
Lizaveta-CR <56049061+Lizaveta-CR@users.noreply.github.com>
2023-11-09 11:00:46 +0300

    Update SOngRepository.java

--- UserRelationsExample/src/main/java/musicApp/repository/SOngRepository.java
+++ UserRelationsExample/src/main/java/musicApp/repository/SOngRepository.java
@@ -2,6 +2,6 @@ package musicApp.repository;
 
 import musicApp.entity.Song;
 import org.springframework.data.repository.PagingAndSortingRepository;
-
+//SongRepostory
 public interface SOngRepository extends PagingAndSortingRepository<Song, Long> {
 }
